d0bd3ae88ba9f6e197309de829f339dc14e797df
Magda <magda.78@hotmail.com>
2020-02-18 19:53:43 -0500

    starting files and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Homepage from './pages/Homepage/Homepage';
+import {Route, Switch} from 'react-router-dom';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+      </Switch>
     </div>
   );
+}
 }
 
 export default App;
